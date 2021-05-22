d1369c0bb7392ad62f7f166c43692edd46e1e13b
Yanis Levesque <yanislevesque@hotmail.fr>
2021-05-22 18:15:38 +0200

    Création et initialisation de la classe Article avec quelques champs

---
+++ app/src/main/java/com/example/newspaper4/Article.kt
@@ -0,0 +1,15 @@
+package com.example.newspaper4
+
+import java.util.*
+
+class Article {
+
+    var title : String? = null
+    var auteur: String? = null
+    var description : String? = null
+    var url : String? = null
+    var urlImage : String? = null
+    var contenu : String? = null
+    var dateParution : Date? = null
+
+}
